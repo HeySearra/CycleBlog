6782dd98fca88dac20f21c39e671f6c741ab335f
tky_hm_xps13 <k_code@163.com>
2020-05-20 20:19:03 +0800

    create utils

--- Cuby/utils/cast_utils.py
+++ Cuby/utils/cast_utils.py
@@ -1,3 +1,11 @@
+import re
 import json
 
+
+def parse_str(s: str):
+    return json.loads(s)
+
+
+def parse_datetime(datetime):
+    return [int(x) for x in filter(lambda s: len(s) <= 4, re.split('[-:/ .]', str(datetime)))]
 
